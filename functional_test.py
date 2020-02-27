34ee3d414bfe0da54f2dddf4025b5616759fc0b6
Vasya115 <leonid802@mail.ru>
2020-02-27 23:01:52 +0300

    improve autotest, add unittest

--- functional_test.py
+++ functional_test.py
@@ -1,8 +1,25 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
 
+        self.browser = webdriver.Firefox()
+    def tearDown(self):
 
-assert 'Django' in browser.title
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+ # Эдит слышала про крутое новое онлайн-приложение со
+ # списком неотложных дел. Она решает оценить его
+ # домашнюю страницу
+        self.browser.get('http://localhost:8000')
+ # Она видит, что заголовок и шапка страницы говорят о
+ # списках неотложных дел
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест!')
+ # Ей сразу же предлагается ввести элемент списка
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
