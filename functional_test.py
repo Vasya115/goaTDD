34cf978d1f45cefe84e665be81284e42a1cc8ea9
Vasya115 <leonid802@mail.ru>
2020-03-05 23:05:28 +0300

    add base view

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 import unittest
 
 
@@ -18,6 +19,25 @@ class NewVisitorTest(unittest.TestCase):
  # Она видит, что заголовок и шапка страницы говорят о
  # списках неотложных дел
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+        inputbox.get_attribute('placeholder'),
+        'Enter a to-do item'
+        )
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+        any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
+
+
         self.fail('Закончить тест!')
  # Ей сразу же предлагается ввести элемент списка
 
